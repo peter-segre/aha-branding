f79d7275cb7f85cca7bf214dbb742abedcbef991
Andy Kauffman <schlefty@gmail.com>
2018-02-27 22:13:37 -0500

    New product logos
    New product logos, header text, and styles

--- portal/custom.js
+++ portal/custom.js
@@ -2,12 +2,7 @@
 
 $('.pureconnect-title').append('PureConnect Ideas');
 
-var pureconnectLogoHtml = '<svg xmlns="http://www.w3.org/2000/svg" class="product-logo-svg" viewBox="0 0 308 208" preserveAspectRation="meet">'
-    + '<path class="product-logo-path" d="M130.19,119.06a18.75,18.75,0,0,0,18.75,18.75h73.17a8.86,8.86,0,0,1,9,8.75h0a8.86,8.86,0,0,1-9,8.75H143.95a13.75,13.75,0,0,1-13.75-13.75h0a13.75,13.75,0,0,1,13.75-13.75h88.74a8.86,8.86,0,0,0,9-8.75h0a8.86,8.86,0,0,0-9-8.75H143.95a13.75,13.75,0,0,1-13.75-13.75h0a13.75,13.75,0,0,1,13.75-13.75h79.83a8.86,8.86,0,0,1,9,8.75h0a8.86,8.86,0,0,1-9,8.75H148.95a18.75,18.75,0,0,0-18.75,18.75Z"></path>'
-    + '<path class="product-logo-path" d="M130.19,174.07a8.75,8.75,0,0,1,8.75-8.75h63.68a8.86,8.86,0,0,1,9,8.75h0a8.86,8.86,0,0,1-9,8.75H138.94a8.75,8.75,0,0,1-8.75-8.75Z"></path>'
-    + '<path class="product-logo-path" d="M66.34,121.5A41.05,41.05,0,0,1,94.17,82.63l65-22.13a8.75,8.75,0,0,0,5.4-11.13h0A8.75,8.75,0,0,0,153.46,44h0L125.93,53.5a12.41,12.41,0,0,0-8.35,11.71l-.12,100.68a10.06,10.06,0,0,1-10.06,10h0a41.05,41.05,0,0,1-41.05-41.05Z"></path>'
-    + '</svg>'
-    + '<h3>Pure<strong>Connect</strong></h3>';
+var pureconnectLogoHtml = '<img class="pureconnect-logo" src="https://genesys.aha.io/attachments/6527300554831414605/token/a5b80efef5f49083f72bc527e72afc74b7b2d718f684def6ca1eef60258937c7" alt="PureConnect Logo">'
 
 $('.pureconnect-logo').append(pureconnectLogoHtml);
 
@@ -15,12 +10,7 @@ $('.pureconnect-logo').append(pureconnectLogoHtml);
 
 $('.pureengage-title').append('PureEngage Ideas');
 
-var pureengageLogoHtml = '<svg xmlns="http://www.w3.org/2000/svg" class="product-logo-svg" viewBox="0 15 308 208" preserveAspectRation="meet">'
-    + '<path class="product-logo-path" d="M155,87.84a11.83,11.83,0,0,1,11.83,11.83v30.88a22.91,22.91,0,0,1-16,21.83,70.81,70.81,0,0,1-7.44,1.94c-11.71,1.72-16.55-1.6-23.11-9.84-9.62-12.08-25-35.88-25-59.22a59.72,59.72,0,1,1,119.45,0c0,23.35-15.4,47.14-25,59.22-6.56,8.24-11.39,11.56-23.11,9.84a70.81,70.81,0,0,1-7.44-1.94,22.91,22.91,0,0,1-16-21.83V99.67A11.83,11.83,0,0,1,155,87.84Z"></path>'
-    + '<rect class="product-logo-path" x="128.24" y="164.61" width="53.48" height="13.26" rx="6.63" ry="6.63"></rect>'
-    + '<rect class="product-logo-path" x="135.15" y="187.59" width="39.66" height="13.26" rx="6.63" ry="6.63"></rect>'
-    + '</svg>'
-    + '<h3>Pure<strong>Engage</strong></h3>';
+var pureengageLogoHtml = '<img class="pureengage-logo" src="https://genesys.aha.io/attachments/6527300554169986560/token/7c948cfbbb95084e4a95aee4d9d63fe4ee46f60083d13f3b196211ffa2653a96" alt="PureEngage Logo">'
 
 $('.pureengage-logo').append(pureengageLogoHtml);
 
@@ -28,11 +18,6 @@ $('.pureengage-logo').append(pureengageLogoHtml);
 
 $('.purecloud-title').append('PureCloud Ideas');
 
-var purecloudLogoHtml = '<svg xmlns="http://www.w3.org/2000/svg" class="product-logo-svg" viewBox="0 0 308 208" preserveAspectRation="meet">'
-    + '<path class="product-logo-path" d="M53.4,128.46h0a27.15,27.15,0,0,1,27.15-27.15h47.58A12.87,12.87,0,0,1,141,114.18h0a12.87,12.87,0,0,1-12.87,12.87H112.59A27.15,27.15,0,0,1,85.45,99.91h0a27.15,27.15,0,0,1,27.15-27.15H184a41.42,41.42,0,0,1,41.42,41.42h0A41.42,41.42,0,0,1,184,155.61H80.55A27.15,27.15,0,0,1,53.4,128.46Z"></path>'
-    + '<rect class="product-logo-path" x="175.98" y="135.73" width="79.4" height="38.94" rx="19.47" ry="19.47"></rect>'
-    + '<rect class="product-logo-path" x="126.21" y="51.7" width="79.4" height="38.94" rx="19.47" ry="19.47"></rect>'
-    + '</svg>'
-    + '<h3>Pure<strong>Cloud</strong></h3>';
+var purecloudLogoHtml = '<img class="purecloud-logo" src="https://genesys.aha.io/attachments/6527313548979542257/token/475cae406c1beddffe4953558e33aead8ab62edb7678066ff390384e1e5b5266" alt="PureCloud Logo">'
 
 $('.purecloud-logo').append(purecloudLogoHtml);
